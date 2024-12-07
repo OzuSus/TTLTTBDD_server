f60a78c6f286daa45448900ccedd5c87b3c62c4a
OzuSus <21130327@st.hcmuaf.edu.vn>
2024-12-07 13:25:08 +0700

    api get product by ID

--- src/main/java/com/TTLTTBDD/server/controllers/ProductController.java
+++ src/main/java/com/TTLTTBDD/server/controllers/ProductController.java
@@ -1,4 +1,31 @@
 package com.TTLTTBDD.server.controllers;
 
+import com.TTLTTBDD.server.models.dto.CategoryDTO;
+import com.TTLTTBDD.server.models.dto.ProductDTO;
+import com.TTLTTBDD.server.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/id")
+    public Optional<ProductDTO> getProductById(@RequestParam int id){
+        return productService.getProductById(id);
+    }
+
 }
--- src/main/java/com/TTLTTBDD/server/repositories/ProductRepository.java
+++ src/main/java/com/TTLTTBDD/server/repositories/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.TTLTTBDD.server.repositories;
 
-public class ProductRepository {
+import aj.org.objectweb.asm.commons.Remapper;
+import com.TTLTTBDD.server.models.entity.Category;
+import com.TTLTTBDD.server.models.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Optional<Product> findProductById(int id);
 }
