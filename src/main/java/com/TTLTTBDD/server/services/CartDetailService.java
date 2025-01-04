009220897d2f578e9a56dda33cc73ae007b4f806
LeThanhTam7723 <21130526@st.hcmuaf.edu.vn>
2025-01-04 18:27:43 +0700

    okee

---
+++ src/main/java/com/TTLTTBDD/server/services/CartDetailService.java
@@ -0,0 +1,83 @@
+package com.TTLTTBDD.server.services;
+
+
+import com.TTLTTBDD.server.models.dto.CartDetailDTO;
+import com.TTLTTBDD.server.models.dto.ProductDTO;
+import com.TTLTTBDD.server.models.entity.Cart;
+import com.TTLTTBDD.server.models.entity.CartDetail;
+import com.TTLTTBDD.server.models.entity.Product;
+import com.TTLTTBDD.server.repositories.CartDetailRepository;
+import com.TTLTTBDD.server.repositories.CartRepository;
+import com.TTLTTBDD.server.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CartDetailService {
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CartDetailRepository cartDetailRepository;
+
+    public CartDetailDTO addProductToCartDetail(Integer idUser, Integer idProduct) {
+        Cart cart = cartRepository.findByIdUser_Id(idUser)
+                .orElseThrow(() -> new IllegalArgumentException("Cart không tồn tại cho User này."));
+
+        Product product = productRepository.findById(idProduct)
+                .orElseThrow(() -> new IllegalArgumentException("Product không tồn tại."));
+
+        CartDetail cartDetail = new CartDetail();
+        cartDetail.setIdCart(cart);
+        cartDetail.setIdProduct(product);
+
+        CartDetail savedCartDetail = cartDetailRepository.save(cartDetail);
+
+        return new CartDetailDTO(
+                savedCartDetail.getId(),
+                savedCartDetail.getIdCart().getId(),
+                savedCartDetail.getIdProduct().getId()
+        );
+    }
+
+    public void removeProductFromCartDetail(Integer idUser, Integer idProduct) {
+        Cart cart = cartRepository.findByIdUser_Id(idUser)
+                .orElseThrow(() -> new IllegalArgumentException("Cart không tồn tại cho User này."));
+
+        CartDetail cartDetail = cartDetailRepository.findByIdCart_IdAndIdProduct_Id(cart.getId(), idProduct)
+                .orElseThrow(() -> new IllegalArgumentException("CartDetail không tồn tại."));
+
+        cartDetailRepository.delete(cartDetail);
+    }
+
+    public List<ProductDTO> getProductsInCartByUserId(Integer idUser) {
+        Cart cart = cartRepository.findByIdUser_Id(idUser)
+                .orElseThrow(() -> new IllegalArgumentException("Cart không tồn tại cho User này."));
+
+        List<CartDetail> cartDetails = cartDetailRepository.findAllByIdCart_Id(cart.getId());
+
+        return cartDetails.stream()
+                .map(cartDetail -> {
+                    Product product = cartDetail.getIdProduct();
+                    return ProductDTO.builder()
+                            .id(product.getId())
+                            .name(product.getName())
+                            .price(product.getPrize())
+                            .quantity(product.getQuantity())
+                            .image(product.getImage())
+                            .description(product.getDescription())
+                            .reviewCount(product.getReview())
+                            .rating(product.getRating())
+                            .categoryID(product.getIdCategory().getId())
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
+}
+
--- src/main/java/com/TTLTTBDD/server/services/UserService.java
+++ src/main/java/com/TTLTTBDD/server/services/UserService.java
@@ -1,19 +1,30 @@
 package com.TTLTTBDD.server.services;
 
 import com.TTLTTBDD.server.models.dto.UserDTO;
+import com.TTLTTBDD.server.models.entity.Cart;
 import com.TTLTTBDD.server.models.entity.User;
+import com.TTLTTBDD.server.repositories.CartRepository;
 import com.TTLTTBDD.server.repositories.UserRepository;
+import com.TTLTTBDD.server.utils.loadFile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CartRepository cartRepository;
+    private loadFile loadFile = new loadFile();
 
     public List<UserDTO> getAllUsers() {
         return userRepository.findAll().stream()
@@ -36,7 +47,39 @@ public class UserService {
             throw new IllegalArgumentException("Username already exists");
         }
         User savedUser = userRepository.save(user);
+        Cart newCart = new Cart();
+        newCart.setIdUser(savedUser);
+        cartRepository.save(newCart);
         return convertToDTO(savedUser);
+    }
+
+    public UserDTO updateUserInfoAccount(UserDTO userDTO) {
+            User user = userRepository.findById(userDTO.getId()).orElseThrow(() -> new RuntimeException("Ko tìm thấy user"));
+            user.setUsername(userDTO.getUsername());
+            user.setFullname(userDTO.getFullname());
+            user.setAddress(userDTO.getAddress());
+            user.setPhone(userDTO.getPhone());
+            user.setEmail(userDTO.getEmail());
+
+            userRepository.save(user);
+            return convertToDTO(user);
+
+    }
+
+    public UserDTO updateUserAvata(UserDTO userDTO, MultipartFile avataFile) {
+        try {
+            if (avataFile != null && !avataFile.isEmpty()) {
+                String avatarPath = loadFile.saveFile(avataFile);
+                userDTO.setAvata(avatarPath);
+            }
+            User user = userRepository.findById(userDTO.getId()).orElseThrow(() -> new RuntimeException("Ko tìm thấy user"));
+            user.setAvata(userDTO.getAvata());
+
+            userRepository.save(user);
+            return convertToDTO(user);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to upload avatar", e);
+        }
     }
 
     private UserDTO convertToDTO(User user) {
